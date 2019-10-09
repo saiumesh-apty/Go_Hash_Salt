2ac0225eb54b3d2625cd351df8b892347df32574
saiumesh-apty <saiumesh.dhanewar@apty.io>
2019-10-09 15:15:05 +0530

    added times for bench marking

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -22,9 +23,8 @@ const (
 	Time         = 1
 	Memory       = 64 * 1024
 	KeyLen       = 32
+	Threads      = 10
 )
-
-var Threads = uint8(runtime.NumCPU())
 
 func generateSalt(len int) (string, error) {
 	unencodedSalt := make([]byte, len)
@@ -35,7 +35,7 @@ func generateSalt(len int) (string, error) {
 }
 
 func generateSaltedHash(password, salt string) (string, error) {
-	cryptoKey := argon2.IDKey([]byte(password), []byte(salt), Time, Memory, Threads, KeyLen)
+	cryptoKey := argon2.IDKey([]byte(password), []byte(salt), Time, Memory, uint8(Threads), KeyLen)
 	encodedPassword := base64.StdEncoding.EncodeToString(cryptoKey)
 	hash := fmt.Sprintf("%s$%d$%d$%d$%d$%s$%s",
 		PasswordType, Time, Memory, Threads, KeyLen, salt, encodedPassword)
@@ -69,19 +69,35 @@ func CompareHashWithPassword(hash, password, salt string) (bool, error) {
 }
 
 func main() {
+
+	fmt.Println("runTime", runtime.NumCPU())
+
+	totalTime := time.Now()
+
+	saltTime := time.Now()
+
 	passwordSalt, err := generateSalt(SaltLength)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("saltTime", time.Since(saltTime))
+
+	hashTime := time.Now()
 	hashedPassword, err := generateSaltedHash("SaltLength", passwordSalt)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("hashTime", time.Since(hashTime))
 	fmt.Println(hashedPassword)
+
+	compareTime := time.Now()
+
 	isPasswordEqual, err := CompareHashWithPassword(hashedPassword, "SaltLength", passwordSalt)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("compareTime", time.Since(compareTime))
+	fmt.Println("totalTime", time.Since(totalTime))
 	fmt.Println(isPasswordEqual) // True
 
 	// wrong password
